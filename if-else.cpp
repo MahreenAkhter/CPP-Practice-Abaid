787ee935586d62644c50233e8d2734a04b8d4b89
Mahreen Akhter <139749436+MahreenAkhter@users.noreply.github.com>
2023-07-17 23:55:05 -0700

    Update if-else.cpp

--- if-else.cpp
+++ if-else.cpp
@@ -1,16 +1,20 @@
 #include<iostream>
 using namespace std;
-int main(){
+int main()
+{
 	int a=20;
 	int b=18;
 	int c=15;
-	if(a<b&&c<b){
+	if(a<b&&c<b)
+	{
 		cout<<"B is greater then A and C";
 	}
-	else if(b<a&&c<a){
+	else if(b<a&&c<a)
+	{
 		cout<<"A is greater then B and C";
 	}
-	else if(a<c&&b<c){
+	else if(a<c&&b<c)
+	{
 		cout<<"C is greater then A and B";
 	}
 	else{
